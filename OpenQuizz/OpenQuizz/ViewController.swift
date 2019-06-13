e8e2ce2681d2f68c3844178d03bf5fc30efe0a11
ohmygymofficial <contact@ohmygym.fr>
2019-06-13 15:19:05 +0200

    CREATE Single View App and dowload files and images

---
+++ OpenQuizz/OpenQuizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by E&M Life Project on 13/06/2019.
+//  Copyright © 2019 Erwan PASTE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
