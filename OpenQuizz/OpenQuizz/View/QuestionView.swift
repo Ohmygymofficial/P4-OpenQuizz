d48a28fae8ba16f6d676785eb003b3fb3248b93f
ohmygymofficial <contact@ohmygym.fr>
2019-06-21 12:51:57 +0200

    FEAT QuestionView with outlet connexion Label and Icon

---
+++ OpenQuizz/OpenQuizz/View/QuestionView.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionView.swift
+//  OpenQuizz
+//
+//  Created by E&M Life Project on 21/06/2019.
+//  Copyright © 2019 Erwan PASTE. All rights reserved.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+
+    @IBOutlet private var label: UILabel!
+    @IBOutlet private var icon: UIImageView!
+}
